29d2a6da0468b8d2662dd30cc2acf234992c48e3
Ravi <ravihalajole@gmail.com>
2026-05-22 13:02:14 +0530

    Add missing Intent and Build class imports to ClipboardActivity
    Operations:
    - Patched: app/src/main/kotlin/org/ravi/codeassist/ClipboardActivity.kt

--- app/src/main/kotlin/org/ravi/codeassist/ClipboardActivity.kt
+++ app/src/main/kotlin/org/ravi/codeassist/ClipboardActivity.kt
@@ -3,6 +3,8 @@ package org.ravi.codeassist
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
